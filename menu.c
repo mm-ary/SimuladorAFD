9660a20f25d9607d7fe2de8a16e817e49b70cae5
alienXXVI <aliana.wkssg@gmail.com>
2024-02-15 19:12:22 -0300

    indicações do menu.c

---
+++ menu.c
@@ -0,0 +1,8 @@
+/*
+Podemos fazer o menu principal, onde a gente pode
+- carregar um novo arquivo de texto
+- testar palavras
+    pode usar "void testar_palavra(GRAMATICA *gramatica, char *palavra)"
+- imprimir a gramatica
+    pode usar "void imprimir_gramatica(GRAMATICA *gramatica)"
+*/
---
+++ readme.txt
@@ -0,0 +1,5 @@
+Equipe:
+Mary (coloca seu nome completo ai)
+Aliana Wakassugui de Paula e Silva
+
+*Daqui pra baixo vão conter instruções simples de como usar o programa, como se fosse um manual de usuário
